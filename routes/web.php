cc386ea151a12bccd384813a3203e6261a7a186d
ZWTao <653668615@qq.com>
2018-10-17 20:55:35 +0800

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::group(['middleware' => 'auth'], function() {
     });
     // 结束
 });
+
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
