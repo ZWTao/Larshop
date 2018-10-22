0447ca27e3c71be80bbdd17c7bd64dee41ee7ce6
ZWTao <653668615@qq.com>
2018-10-22 18:46:27 +0800

    微信支付

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
 
         'payment/alipay/notify',
+        'payment/wechat/notify',
         //
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,8 @@ Route::group(['middleware' => 'auth'], function() {
 
         Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
 
+        Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
+
     });
     // 结束
 });
@@ -65,3 +67,4 @@ Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
+Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
